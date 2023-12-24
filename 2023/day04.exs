0f013c43c0fdcf7a87e46a81d5221d85f3afe1b9
zughcbmlgfxsjjyznz <zughcbmlgfxsjjyznz@cwmxc.com>
2023-12-24 10:27:25 +0000

    feat: day 4 part 2

--- 2023/day04.exs
+++ 2023/day04.exs
@@ -1,12 +1,9 @@
 defmodule DayFour do
   def points(input) do
     File.stream!(input)
-    |> parse()
+    |> Enum.map(&parse(&1))
     |> Enum.reduce(0, fn %{winners: winners, candidates: candidates}, sum ->
-      count =
-        candidates
-        |> Enum.filter(fn c -> Enum.member?(winners, c) end)
-        |> Enum.count()
+      count = candidates |> Enum.count(fn c -> Enum.member?(winners, c) end)
 
       case count == 0 do
         true -> sum
@@ -15,31 +12,68 @@ defmodule DayFour do
     end)
   end
 
-  def parse(lines) do
-    lines
-    |> Enum.map(fn line ->
-      numbers =
-        line
-        |> String.trim()
-        |> String.split(":")
-        |> Enum.at(1)
-        |> String.split("|")
+  def stacks(input) do
+    cards =
+      File.stream!(input)
+      |> Enum.map(&parse(&1))
 
-      scan = fn str ->
-        Regex.scan(~r/(\d+)/, str, capture: :all_but_first)
-        |> List.flatten()
-        |> Enum.map(&String.to_integer(&1))
+    decks =
+      1..Enum.count(cards)
+      |> Enum.reduce(%{}, fn c, acc -> Map.put(acc, c, 1) end)
+
+    cards
+    |> Enum.with_index(1)
+    |> Enum.reduce(decks, fn {%{winners: winners, candidates: candidates}, card}, acc ->
+      count = candidates |> Enum.count(fn c -> Enum.member?(winners, c) end)
+
+      first = card + 1
+      last = card + count
+
+      cond do
+        first <= last ->
+          to_add = Map.get(acc, card)
+
+          first..last
+          |> Enum.reduce(acc, fn i, sub_acc ->
+            existing = Map.get(sub_acc, i)
+
+            Map.put(sub_acc, i, existing + to_add)
+          end)
+
+        true ->
+          acc
       end
-
-      %{
-        winners: Enum.at(numbers, 0) |> scan.(),
-        candidates: Enum.at(numbers, 1) |> scan.()
-      }
     end)
+    |> Enum.map(fn {_, v} -> v end)
+    |> Enum.sum()
+  end
+
+  def parse(line) do
+    numbers =
+      line
+      |> String.trim()
+      |> String.split(":")
+      |> Enum.at(1)
+      |> String.split("|")
+
+    scan = fn str ->
+      Regex.scan(~r/(\d+)/, str, capture: :all_but_first)
+      |> List.flatten()
+      |> Enum.map(&String.to_integer(&1))
+    end
+
+    %{
+      winners: List.first(numbers) |> scan.(),
+      candidates: List.last(numbers) |> scan.()
+    }
   end
 end
 
 # 13
 IO.inspect(DayFour.points("2023/day04_example"))
-# ?
+# 25010
 IO.inspect(DayFour.points("2023/day04_input"))
+# 30
+IO.inspect(DayFour.stacks("2023/day04_example"))
+# 9924412
+IO.inspect(DayFour.stacks("2023/day04_input"))
